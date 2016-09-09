ee1501c912d0e6c6be8710e1ef2ce4427f3a7cde
Dwayne Macadangdang <dwayne.macadangdang@gmail.com>
2016-09-09 16:56:30 -0700

    Formatting

--- plot1.R
+++ plot1.R
@@ -7,18 +7,18 @@ plot1 <- function()
 {
   setwd("/Users/mistermaxx/Documents/work/personal/Coursera/Exp_Data_Anal/Week_4")
   
-  # read in data from files
+  # read in data from file
   nei.file.data <- readRDS("summarySCC_PM25.rds")
-  scc.file.data <- readRDS("Source_Classification_Code.rds")
+  #scc.file.data <- readRDS("Source_Classification_Code.rds")
   
   # aggregate total pm2.5 emissions from all sources, years 1999, 2002, 2005 2008
   aggregate.data <- aggregate(Emissions ~ year, nei.file.data, sum)
-  
   
   # graph the emission data, save file
   barplot((aggregate.data$Emissions)/10^6, names.arg = aggregate.data$year, xlab="Year",
     ylab = "PM2.5 Emissions (10^6 Tons)", main = "Total PM2.5 Emissions From All US Sources")
   
-  dev.copy(png, file="plot1.png")
+  dev.copy(png, file = "plot1.png")
   dev.off()
+  
 }
