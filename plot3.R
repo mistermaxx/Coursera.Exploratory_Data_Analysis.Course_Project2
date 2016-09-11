7ad93da22c97e96874d0d00671109a5ba6dd49bb
Dwayne Macadangdang <dwayne.macadangdang@gmail.com>
2016-09-11 13:02:07 -0700

    clean up

--- plot3.R
+++ plot3.R
@@ -10,15 +10,9 @@ plot3 <- function()
   
   # read in data from file
   nei.file.data <- readRDS("summarySCC_PM25.rds")
-  #scc.file.data <- readRDS("Source_Classification_Code.rds")
   
   # subset and aggregate total pm2.5 emissions for baltimore
   nei.baltimore.data <- nei.file.data[nei.file.data$fips == "24510",]
-  #aggregate.baltimore.data <- aggregate(Emissions ~ year, nei.baltimore.data, sum)
-  
-  # graph the emission data, save file
-  #barplot(aggregate.baltimore.data$Emissions, names.arg = aggregate.baltimore.data$year, xlab = "Year", ylab = "PM2.5 Emissions (Tons)", main = "Total PM2.5 Emissions For Baltimore, All Sources")
-
   
   ggplot.baltimore <- ggplot(nei.baltimore.data, aes(factor(year), Emissions, fill = type)) +
     geom_bar(stat = "identity") +
